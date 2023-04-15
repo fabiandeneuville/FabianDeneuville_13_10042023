89d1e634fcb968d1db0c2b2f13a1370d3dcdf135
Fabian Deneuville <deneuville.fabian@live.fr>
2023-04-15 21:17:46 +0200

    Getting user data and making state persistent

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import './style/main.scss';
 import router from './router';
-import { RouterProvider, useLocation } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { isUserLogged, isTokenValid, getToken } from "./utils";
 import { getUserDataStarted } from './store/action';
-
-// const App = () => <RouterProvider router={router} />
 
 function App(){
 
     const dispatch = useDispatch();
 
     useEffect(() => {
-        if(isUserLogged && isTokenValid){
+        if(isUserLogged() && isTokenValid()){
             const token = getToken();
             dispatch(getUserDataStarted(token));
         }
