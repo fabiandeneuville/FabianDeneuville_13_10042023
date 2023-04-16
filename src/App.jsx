fc032ce7c05a0d8d8298bbea293b963c90caacab
Fabian Deneuville <deneuville.fabian@live.fr>
2023-04-16 21:58:57 +0200

    Moving token checking from App to Layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,12 @@
 import './style/main.scss';
 import router from './router';
 import { RouterProvider } from 'react-router-dom';
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { isUserLogged, isTokenValid, getToken } from "./utils";
-import { getUserDataStarted } from './store/action';
-
-import Loader from './components/Loader';
 
 function App(){
 
-    const isRequestion = useSelector(state => state.isRequesting)
-    
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        if(isUserLogged() && isTokenValid()){
-            const token = getToken();
-            dispatch(getUserDataStarted(token));
-        }
-    }, []);
-
     return (
         <div>
-            {isRequestion &&
-                <Loader/>
-            }
             <RouterProvider router={router} />
-
         </div>
     )
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,31 @@
 import {Outlet} from "react-router-dom";
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { isUserLogged, isTokenValid, getToken } from "../utils";
+import { getUserDataStarted } from '../store/action';
 
 import Header from "./Header";
 import Footer from "./Footer";
+import Loader from "./Loader";
 
 function Layout(){
+
+    const isRequestion = useSelector(state => state.isRequesting)
+    
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if(isUserLogged() && isTokenValid()){
+            const token = getToken();
+            dispatch(getUserDataStarted(token));
+        }
+    }, []);
+
     return (
         <div className="layout">
+            {isRequestion &&
+                <Loader/>
+            }
             <Header/>
                 <Outlet/>
             <Footer/>
