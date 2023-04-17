d1677dd623505a17e4f2146e6e4c8cf77af25d90
Fabian Deneuville <deneuville.fabian@live.fr>
2023-04-17 21:07:01 +0200

    Adding not found page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 function NotFound(){
     return (
-        <main>
-            <h1>404</h1>
+        <main className="main bg-dark notFound">
+            <h1 className="notFound__heading">404</h1>
+            <p className="notFound__text">Oops! Looks like you've reached a page that doesn't exist.</p>
+            <Link className="button notFound__button" to="/">Home</Link>
         </main>
     );
 };
